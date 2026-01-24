513d21ba229df1b6dd96392f02c27f7311a60653
Uditya <uditya204@gmail.com>
2026-01-24 18:41:04 +0530

    initialize

---
+++ src/app/(tabs)/_layout.tsx
@@ -0,0 +1,47 @@
+import { useColorScheme } from "@/components/useColorScheme";
+import Colors from "@/constants/Colors";
+import { Link, Tabs } from "expo-router";
+import React from "react";
+import { Pressable } from "react-native";
+import { House, User, MessageCircleMore  } from 'lucide-react-native';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Campus Ride",
+          tabBarIcon: ({ color }) => <House size={23} color={color} />,
+          headerRight: () => (
+            <Link href="/message" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <MessageCircleMore
+                    size={25}
+                    color={Colors[colorScheme ?? "light"].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => <User size={23} color={color}/>,
+        }}
+      />
+    </Tabs>
+  );
+}
